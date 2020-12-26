9267d493fbd679711d22aa1b52845a015c7e2f69
Sachin Nimshan <sachinnimshan@gmail.com>
2020-12-26 11:48:04 +0530

    latest

--- src/Products/HomeScreen.js
+++ src/Products/HomeScreen.js
@@ -1,28 +1,19 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import Products from './Products';
 import './HomeScreen.css';
 import axios from 'axios';
 import LoadingBox from '../Components/LoadingBox';
 import MessageBox from '../Components/MessageBox';
+import { ListProducts } from '../Actions/ProductActions';
+import {useSelector, useDispatch} from 'react-redux';
 
 function HomeScreen() {
-    const [products, setproducts] = useState([]);
-    const [loading, setloading] = useState(false);
-    const [error, seterror] = useState(false);
+    const dispatch = useDispatch();
+    const ProductList = useSelector((state) => state.ProductList);
+    const {loading, error, products}= ProductList;
 
     useEffect(()=>{
-        const FetchData = async() => {
-        try{
-        setloading(true);    
-        const {data} = await axios.get('/api/products');
-        setloading(false);
-        setproducts(data);
-        }catch(err){
-        seterror(err.message);
-        setloading(false);
-        }
-        };
-        FetchData();
+        dispatch(ListProducts());
     }, []);
 
     return (
--- src/Store.js
+++ src/Store.js
@@ -1,12 +1,13 @@
 import Data from './Data';
-import {createStore, compose, applyMiddleware} from 'redux';
+import {createStore, compose, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
+import {ProductListReducer} from './Reducers/ProductReducer';
 
 const initialState = {};
 
-const reducer = (state, action)=>{
-    return {products: Data.ProductData};
-}
+const reducer = combineReducers({
+    ProductList: ProductListReducer,
+});
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore
