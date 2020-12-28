6126210042aa506e70e94e0859eec86c574e16f5
Sachin Nimshan <sachinnimshan@gmail.com>
2020-12-28 10:08:12 +0530

    latest

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Footer.css';
+import {Link} from 'react-router-dom';
 
 function Footer() {
 
@@ -12,14 +13,46 @@ function Footer() {
             <div className='top-footer-container' onClick={scrollTop}>
                 <span>Back To Top</span> 
             </div>
-
-            <div className='middle-footer-container'>
-                <span>SN</span>
-
-            </div>
-
             <div className='bottom-footer-container'>
+                <div className='bottom-footer-column'>
+                 <ul>
+                     <li><span className='footer-column-title'>Contact Us</span></li>
+                     <li><span className='footer-details'>sachinnimshan@gmail.com</span></li>
+                     <li><span className='footer-details'>Follow us on Social Media</span></li>
+                     <li><span className='footer-details'>Download our Free App</span></li>
+                 </ul>
+                </div>
 
+                <div className='bottom-footer-column'>
+                 <ul>
+                     <li><span className='footer-column-title'>Information</span></li>
+                     <li><Link className='footer-links'>About Us</Link></li>
+                     <li><Link className='footer-links'>Terms and Conditions</Link></li>
+                     <li><Link className='footer-links'>Privacy Policy</Link></li>
+                     <li><Link className='footer-links'>FAQ</Link></li>
+                     <li><Link className='footer-links'>Contact Us</Link></li>                  
+                 </ul>
+                </div>
+
+                <div className='bottom-footer-column'>
+                 <ul>
+                     <li><span className='footer-column-title'>My Account</span></li>
+                     <li><Link className='footer-links'>Sign Up</Link></li>
+                     <li><Link className='footer-links'>My Account</Link></li>
+                     <li><Link className='footer-links'>My Vouchers</Link></li>
+                     <li><Link className='footer-links'>My Wishlist</Link></li>
+                 </ul>
+                </div>
+
+                <div className='bottom-footer-column'>
+                 <ul>
+                     <li><span className='footer-column-title'>Newsletter</span></li>
+                     <li><p className='footer-details'>
+                     Signup To Our Newsletter</p></li>
+                     <li><input placeholder='Your Email' className='newsletter-field'/>
+                     <button className='newsletter-btn'>Subscribe</button></li>
+                 </ul>
+                </div>
             </div>
             
         </div>
