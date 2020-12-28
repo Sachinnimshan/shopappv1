4efa9b2a92c2c6c1568ddd38e8ae356f1cc52245
Sachin Nimshan <sachinnimshan@gmail.com>
2020-12-28 08:03:03 +0530

    latest

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './Footer.css';
 
 function Footer() {
+
+    const scrollTop = () =>{
+        window.scrollTo({top: 0, behavior: 'smooth'});
+      };
+
     return (
         <div className='main-footer-container'>
-            <div className='top-footer-container'>
+            <div className='top-footer-container' onClick={scrollTop}>
                 <span>Back To Top</span> 
             </div>
 
