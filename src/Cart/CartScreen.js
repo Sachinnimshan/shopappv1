2723686677ef5dd53e7de077a75795547ade7857
Sachin Nimshan <sachinnimshan@gmail.com>
2021-01-01 10:00:26 +0530

    latest

--- src/Cart/CartScreen.js
+++ src/Cart/CartScreen.js
@@ -1,6 +1,9 @@
 import React, {useEffect} from 'react';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import { AddToCart } from '../Actions/CartActions';
+import {Card} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+import './CartScreen.css';
 
 function CartScreen(props) {
     const dispatch = useDispatch();
@@ -14,14 +17,48 @@ function CartScreen(props) {
         }
     }, [dispatch, ProductID, qty]);
 
+    const Cart = useSelector(state=> state.Cart);
+    const{CartItems}= Cart;
+
     return (
         <div className='main-cart-container'>
             <div className='cart-items-container'>
-                <h1>Sachin Nimshan</h1>
+                {(CartItems.length === 0) ? (
+                    <Card className='empty-cart-card-container'>
+                        <div><img src='/Images/cartempty.png'/></div>
+                        <div>
+                            <span>Your Shopping Cart is Empty</span>
+                            <span>
+                                <button>Sign in to your account</button>
+                                <button>Sign up now</button>
+                            </span>
+                        </div>                
+                    </Card>
+                ): (
+                    <ul>
+                        {CartItems.map((item)=>(
+                            <Card key={item.product} className='cart-list-container'>
+                                <div><Card.Img className='cart-list-product-image' 
+                                src={item.Image}/></div>
+                                <Card.Body>
+                                <div><Card.Title>
+                                <Link to={`/product/${item.product}`}>{item.Title}</Link>
+                                </Card.Title></div>
+                                </Card.Body>
+                            </Card>
+                        ))}
+                    </ul>
+                    
+                )
+            }
+                
 
             </div>
 
             <div className='cart-total-container'>
+                <Card>
+
+                </Card>
 
             </div>
             
